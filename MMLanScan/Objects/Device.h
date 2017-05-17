dcccca95aaa2dee4a6cf2ef4b4d3c89bc6dc93ec
Kain <evgeniy@macsoftex.com>
2017-05-17 15:18:43 +0300

    no message

--- MMLanScan/Objects/Device.h
+++ MMLanScan/Objects/Device.h
@@ -14,6 +14,7 @@
 @property (nonatomic,strong) NSString *macAddress;
 @property (nonatomic,strong) NSString *subnetMask;
 @property (nonatomic,strong) NSString *brand;
+@property (nonatomic,strong) NSData *rawAddress;
 
 -(NSString*)macAddressLabel;
 @end
